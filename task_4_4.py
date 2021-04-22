11b4446c5a94c9ff41132f699ba55182a19686d3
Maksim Veselov <veselow.me@gmail.com>
2021-04-22 15:07:01 +0300

    Домашняя работа № 4

---
+++ task_4_4.py
@@ -0,0 +1,5 @@
+import utils
+
+print("1 Доллар США равен", utils.currency_rates('usd'), "руб.")
+print("1 Евро равен", utils.currency_rates('eur'), "руб.")
+print("1 Белорусский рубль равен", utils.currency_rates('byn'), "руб.")
